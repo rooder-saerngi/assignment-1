5f2bb67e85fe9f24e293bed44292d7e534fcadc9
rooder-saerngi <abhinabbadal11@gmail.com>
2025-12-04 11:31:56 +0545

    Update traffic_generator.py

--- traffic_generator.py
+++ traffic_generator.py
@@ -22,10 +22,44 @@ lanes = {
     "CL1": Queue(), "CL2": Queue(), "CL3": Queue(),
     "DL1": Queue(), "DL2": Queue(), "DL3": Queue(),
 }
-#Traffic Lights
-traffic_light = {
-    "A": "RED",
-    "B": "RED",
-    "C": "RED",
-    "D": "RED"
-}
+#Traffic Lights (every ten seconds the lights change )
+def Lights_Changer():
+    Flag = True
+    while True :
+        if Flag == True :
+            LaneA_lights = "GREEN"
+            LaneB_Lights = "RED"
+            LaneD_lights = "RED"
+            LaneC_lights = "GREEN"
+
+            Flag = False
+            time.sleep(10)
+        else :
+            LaneA_lights = "RED"
+            LaneB_Lights = "GREEN"
+            LaneD_lights = "GREEN"
+            LaneC_lights = "RED"
+            Flag = True
+            time.sleep(10)
+
+
+def generator():
+    global i
+    i =0
+    while True :
+        #for the non priority lanes (cars are generated every 10 seconds)
+        lanes["Al3"].enqueue(f"car_AL3_{i}")
+        lanes["BL3"].enqueue(f"car_BL3_{i}")
+        lanes["CL3"].enqueue(f"car_CL3_{i}")
+        lanes["DL3"].enqueue(f"car_DL3_{i}")
+
+        time.sleep(10)
+
+        #for the priority lanes (cars are generated every 5 seconds)
+
+        lanes["AL2"].enqueue(f"car_AL2_{i}")
+        lanes["BL2"].enqueue(f"car_BL2_{i}")
+        lanes["CL2"].enqueue(f"car_CL2_{i}")
+        lanes["DL2"].enqueue(f"car_DL2_{i}")
+
+        i += 1
