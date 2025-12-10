b30becd8193f789ac0ee3b74bbfbe9555b4450fc
rooder-saerngi <abhinabbadal11@gmail.com>
2025-12-10 23:41:51 +0545

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,1 +1,17 @@
+import pygame
+import sys
 
+from pygame.examples.moveit import WIDTH, HEIGHT
+
+pygame.init()
+
+WIDTH,HEIGHT = 1280 , 720
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption("Traffic Simulator")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
