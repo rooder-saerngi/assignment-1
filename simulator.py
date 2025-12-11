b361ac3d36569d4d4e30d96451756e6c0ba4e13d
rooder-saerngi <abhinabbadal11@gmail.com>
2025-12-11 12:27:04 +0545

    Update simulator.py
    background image with the road is set

--- simulator.py
+++ simulator.py
@@ -1,17 +1,30 @@
 import pygame
 import sys
-
-from pygame.examples.moveit import WIDTH, HEIGHT
+import random
 
 pygame.init()
 
 WIDTH,HEIGHT = 1280 , 720
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption("Traffic Simulator")
+pygame.mouse.set_visible(0)
 
+bg = pygame.image.load("Road.png")
+#Making sure that the exit button works
+
+clock = pygame.time.Clock()
+FPS = 60
 while True:
     for event in pygame.event.get():
+
+        screen.blit(bg, (0, 0))
+
+        pygame.display.update()
+
+        clock.tick(FPS)
+
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
